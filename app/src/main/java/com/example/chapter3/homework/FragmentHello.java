b7ce3910c74678182904f394258715006d55626b
LSXCH <51190956+LSXCH@users.noreply.github.com>
2019-07-11 02:13:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chapter3/homework/FragmentHello.java
@@ -0,0 +1,22 @@
+package com.example.chapter3.homework;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentHello extends Fragment {
+
+    private static final String TAG = "HelloFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hello, container, false);
+    }
+}
